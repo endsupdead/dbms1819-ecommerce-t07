0a8021dc195c4bcb939452797f5165b831bef369
fifixme <you@example.com>
2018-08-07 21:37:16 +0800

    module 2

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ client.connect()
 	})
 
 /*
-CREATE TABLE products_m1(id SERIAL PRIMARY KEY, name varchar(80), type varchar(80), description varchar(300), brand varchar(80), price float(2), pic varchar(80));
+CREATE TABLE products_m1(id SERIAL PRIMARY KEY, name varchar(80), type varchar(80), description varchar(300), brand varchar(80), price float(10), pic varchar(80));
 INSERT INTO products_m1(name, type, description, brand, price, pic) VALUES('HP ZBook Studio x360 G5', 'Laptop', '8th Generation Intel Core i7 processor with 16 GB memory and 512 GB SSD', 'HP', '142812.19', '/one.png');
 INSERT INTO products_m1(name, type, description, brand, price, pic) VALUES('HP EliteBook x360 1020 G2', 'Laptop', '7th Generation Intel Core i5 processor with 8 GB memory and 128 GB SSD storage', 'HP', '76670.94','/two.png');
 INSERT INTO products_m1(name, type, description, brand, price, pic) VALUES('HP mt21 Mobile Thin Client', 'Laptop', 'Intel® Celeron® 3865u with Intel HD Graphics with 4 GB DDR$-2400 SDRAM', 'HP', '25874.46','/three.png');
@@ -41,8 +41,7 @@ INSERT INTO products_m1(name, type, description, brand, price, pic) VALUES('HP 2
 INSERT INTO products_m1(name, type, description, brand, price, pic) VALUES('HP Stream 11 Pro G4 EE Notebook PC', 'Laptop','Intel® Celeron® processor with 4 GB memory and 128 GB eMMC storage', 'HP', 25345.33,'/six.png');
 
 
-
-INSERT INTO products_m1(name, type, description, brand, price, pic) VALUES('ZBook Studio x360 G5', 'Laptop', '8th Generation Intel Core i7 processor with 16 GB memory and 512 GB SSD', 'HP', '142812.19', '/one.png'), ('EliteBook x360 1020 G2', 'Laptop', '7th Generation Intel Core i5 processor with 8 GB memory and 128 GB SSD storage', 'HP', '76670.94','/two.png'), ('mt21 Mobile Thin Client', 'Laptop', 'Intel® Celeron® 3865u with Intel HD Graphics with 4 GB DDR$-2400 SDRAM', 'HP', '25874.46','/three.png'), ('ProBook x360 11 G2 EE Notebook PC', 'Laptop','7th Generation Intel® Core™ m3 processor with 4 GB memory and 128 GB SSD storage', 'HP', 40160.97,'/four.png'), ('255 G6 Notebook PC ', 'Laptop','AMD A6 APU with 8 GB memory and 256 GB SSD storage', 'HP', 27461.85,'/five.png'), ('Stream 11 Pro G4 EE Notebook PC', 'Laptop','Intel® Celeron® processor with 4 GB memory and 128 GB eMMC storage', 'HP', 25345.33,'/six.png');
+INSERT INTO products_m1(name, type, description, brand, price, pic) VALUES('ZBook Studio x360 G5', 'Laptop', '8th Generation Intel Core i7 processor with 16 GB memory and 512 GB SSD', 'HP', '142812.19', '/one.png'), ('EliteBook x360 1020 G2', 'Laptop', '7th Generation Intel Core i5 processor with 8 GB memory and 128 GB SSD storage', 'HP', '76670.94','/two.png'), ('Nitro 5', 'Laptop', 'Intel Core i7 with DDR$ 8GB memory and 256 GB SSD', 'Acer', '72999', '/three.png'), ('Aspire E1-572G', 'Laptop', 'Intel Core i7 with DDR3 4GB RAM and 1TB Storage', 'Acer', '37990', '/four.png'), ('GL63 8RC', 'Laptop', 'Intel Core i7 with 8GB RAM and 1TB Storage', 'MSI', '84000', '/five.png'), ('GV62 7RD', 'Laptop', 'Intel Core i7 with 8GB RAM and 2TB Storage', 'MSI', '74000', '/six.png');
 
 */
 //View engine setup
@@ -65,7 +64,7 @@ app.get('/', function(req,res) {
 		}
 		res.render('home',{
 			data: list,
-			title: 'HP Products'
+			title: 'Lappy Products'
 		});
 	});
 });
@@ -85,6 +84,20 @@ app.get('/products/:id', (req,res)=>{
 	});
 });
 
+app.get('/brands', function(req,res){
+	res.render('brand_create',{
+
+	});
+});
+app.get('/categories', function(req,res){
+	res.render('category_create',{
+
+	});
+});
+app.get('/brand/create', function(req, res) {
+	res.render('create_brand',{
+	});
+})
 app.get('/edit', (req,res)=>{
 	var id = req.params.id;
 	res.render('edit');
@@ -151,6 +164,36 @@ app.post('/products/:id/send', function(req, res) {
      });
 });
 
+app.get('/product/create', function(req, res) {
+	 var category = []; 
+	 var brand = [];
+	 var both =[];
+	 client.query('SELECT * FROM products_brand')
+	.then((result)=>{
+	    brand = result.rows;
+	    console.log('brand:',brand);
+	     both.push(brand);
+	})
+	.catch((err) => {       
+		console.log('error',err);
+		res.send('Error!');
+	});
+    client.query('SELECT * FROM products_category')
+	.then((result)=>{
+	    category = result.rows;
+	    both.push(category);
+	    console.log(category);
+	    console.log(both);
+		res.render('create_product',{
+			rows: both
+		});
+	})
+	.catch((err) => {
+		console.log('error',err);
+		res.send('Error!');
+	});
+
+});
 app.listen(5000,function() {
 	console.log('Server started at port 5000');
 });
